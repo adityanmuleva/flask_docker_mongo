2694d3dbf4f3f9148ad7e7ae767aba9c7cfeb921
aaditya_muleva <aaditya_muleva@dell.com>
2022-08-03 15:56:09 +0530

    adding uri

--- mongo_config.py
+++ mongo_config.py
@@ -1,5 +1,16 @@
 from pymongo import MongoClient
+import os
 
 myclient = MongoClient("mongodb://localhost:27017/")
 my_db = myclient["Dockerdb"]
 my_collection = my_db["my_col"]
+
+# client = MongoClient('example.com',
+#                      username='user',
+#                      password='password',
+#                      authSource='the_database',
+#                      authMechanism='SCRAM-SHA-256')
+
+uri = os.environ["MONGO_DB_URI"]
+# uri = "mongodb://user:password@example.com/?authSource=the_database&authMechanism=SCRAM-SHA-256"
+client = MongoClient(uri)
